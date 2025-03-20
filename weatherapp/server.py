a3ec5a268e26f1093a1b50040ce28a266a76469a
Sergey Simakov <SaneniRem@yandex.ru>
2025-03-20 17:27:26 +0300

    Шаг 3.3 Добавлены exception handler + логгирование

--- weatherapp/server.py
+++ weatherapp/server.py
@@ -1,27 +1,40 @@
-from flask import Flask
+import logging
+from flask import Flask, jsonify
 from weatherapp.weather import get_weather_by_city
 from weatherapp.config import load_from_env
 import dataclasses as dc
 
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    filename="app.log",
+    filemode="w",
+    encoding="utf-8"
+)
+logger = logging.getLogger(__name__)
+
 conf = load_from_env()
 app = Flask(__name__)
-
 
 @dc.dataclass
 class WeatherResult:
     temp: float
     feels: float
 
-
 @app.route("/")
 def index():
-    try:
-        weather = get_weather_by_city("Moscow,Russia", conf.token)
-        weather_result = WeatherResult(
-            temp=float(weather['temp_C']),
-            feels=float(weather['FeelsLikeC'])
-        )
-        return dc.asdict(weather_result)
-    except Exception as error:
-        return {"error": str(error)}
+    weather = get_weather_by_city("Moscow,Russia", conf.token)
+    weather_result = WeatherResult(
+        temp=float(weather['temp_C']),
+        feels=float(weather['FeelsLikeC'])
+    )
+    return dc.asdict(weather_result)
+
+
+@app.errorhandler(Exception)
+def handle_exception(error):
+    error_message = str(error)
+    logger.exception(f"Глобальная ошибка: {error_message}")
+    return {"error": error_message}, 500
