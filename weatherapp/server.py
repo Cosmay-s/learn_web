c18213d5c1c58f4347a516b665db9ec92aa4b63e
Sergey Simakov <SaneniRem@yandex.ru>
2025-03-20 16:57:10 +0300

    Шаг 3.2 - Добавлен датакласс для вывода

--- weatherapp/server.py
+++ weatherapp/server.py
@@ -1,19 +1,27 @@
 from flask import Flask
 from weatherapp.weather import get_weather_by_city
 from weatherapp.config import load_from_env
+import dataclasses as dc
 
 
 conf = load_from_env()
-
 app = Flask(__name__)
+
+
+@dc.dataclass
+class WeatherResult:
+    temp: float
+    feels: float
+
 
 @app.route("/")
 def index():
     try:
         weather = get_weather_by_city("Moscow,Russia", conf.token)
-        return {
-            "temp": float(weather['temp_C']),
-            "feels": float(weather['FeelsLikeC'])
-        }
+        weather_result = WeatherResult(
+            temp=float(weather['temp_C']),
+            feels=float(weather['FeelsLikeC'])
+        )
+        return dc.asdict(weather_result)
     except Exception as error:
         return {"error": str(error)}
